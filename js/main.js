6f5be02e16ee207b9ac9d7d95f84f6e5a3660064
CJEF <61284486+CJEF@users.noreply.github.com>
2020-07-17 16:42:13 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,44 @@
-$(document).ready(function(){
-  $('.slider').slick({
-    // infinite: false
+$(document).ready(function () {
+  $(".slider").slick({
+    infinite: false,
+    dots: true
   });
 
+  $(".accordion__title").on("click", function () {
+    $(".accordion__item").removeClass("accordion__item--active");
+    $(this).parent().addClass("accordion__item--active");
+  });
 
-  $('.accordion__title').on('click', function () {
-    $('.accordion__item').removeClass('accordion__item--active')
-    $(this).parent().addClass('accordion__item--active')
-});
+  var block = $("#sidebar"); // определяем элемент, к которому будем применять условия (можем указывать ID, класс либо любой другой идентификатор элемента)
 
-var block = $("#sidebar");// определяем элемент, к которому будем применять условия (можем указывать ID, класс либо любой другой идентификатор элемента)
+  $(".burger").click(function () {
+    block.addClass("active");
+  });
 
-$('.burger').click(function() {
-    block.addClass('active');
-});
+  $(document).mouseup(function (e) {
+    // отслеживаем событие клика по веб-документу
 
-$(document).mouseup(function (e){ // отслеживаем событие клика по веб-документу
-     
-    if (!block.is(e.target) // проверка условия если клик был не по нашему блоку
-        && block.has(e.target).length === 0) { // проверка условия если клик не по его дочерним элементам
-        block.removeClass('active'); // если условия выполняются - скрываем наш элемент
+    if (
+      !block.is(e.target) && // проверка условия если клик был не по нашему блоку
+      block.has(e.target).length === 0
+    ) {
+      // проверка условия если клик не по его дочерним элементам
+      block.removeClass("active"); // если условия выполняются - скрываем наш элемент
     }
-});
+  });
 
+  $(".default-option").click(function () {
+    $(this).parent().toggleClass("active");
+  });
+
+  $(".select-ul li").click(function () {
+    let currentelem = $(this).html();
+    $(".default-option li").html(currentelem);
+    $(this).parents(".select_wrap").removeClass("active");
+  });
+
+  $('.tab').click(function() {
+    $('.tab-item').removeClass('active');
+    $(this).parent().addClass("active");
+  })
 });
